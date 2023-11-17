e64737748292400914b87519e30c3e934bfe4bcf
Anton Ödman <anton.odman@gmail.com>
2023-11-17 09:00:07 +0100

    chore: reset debug file

--- src/runtime.ts
+++ src/runtime.ts
@@ -1,7 +1,12 @@
+import { LOG_FILE_DIRECTORY } from "@/constants";
+import { FileService } from "@/services/file-service";
+
 let DEBUG = false;
 
-export const setDebug = (value: boolean) => (DEBUG = value);
-export const isDebug = () => DEBUG;
-export const isDev = () => {
-    return process.env.NODE_ENV === "development";
+export const setDebug = (value: boolean) => {
+    DEBUG = value;
+    FileService.removeFile(LOG_FILE_DIRECTORY);
+    FileService.assureFile(LOG_FILE_DIRECTORY);
 };
+export const isDebug = () => DEBUG;
+export const isDev = () => process.env.NODE_ENV === "development";
